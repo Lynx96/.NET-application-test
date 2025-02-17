4044c708a084bcb59591f29ec4a5d33e021a8535
Lynx96 <joaohenrique_as@hotmail.com>
2025-02-17 17:10:55 -0300

    launchSettings applicationURL altered to localhost on port 5000

--- Server/TestApplication/obj/Debug/net8.0/TestApplication.AssemblyInfo.cs
+++ Server/TestApplication/obj/Debug/net8.0/TestApplication.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc6862db1ba3d9d7683a8d99b1f4fb4f3853d0c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9630ca381507b3c34342eac3dcd4bf32cffef088")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
